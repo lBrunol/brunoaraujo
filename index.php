a368d2dd3d199648e7e01668a06709ef92924312
Bruno Alves <bruno.ar.avl@gmail.com>
2020-01-16 15:23:22 -0300

    inicio da estilização da home

--- index.php
+++ index.php
@@ -15,45 +15,28 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area container-small">
 		<main id="main" class="site-main">
-
+		<!-- Página com o conteúdo descritivo da home -->
 		<?php
-		if ( have_posts() ) :
+			$args = [
+				'name' => 'apresentacao-home',
+				'post_type' => 'page',
+				'numberposts' => 1	
+			];
+			$arr_pages = get_posts($args);
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+			if(count($arr_pages) > 0) :
+				$page_apresentacao = array_shift($arr_pages); ?>
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
+				<article class="home-presentation">
+					<?php echo $page_apresentacao->post_content; ?>
+				</article>
+		<?php
+			endif 
 		?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
